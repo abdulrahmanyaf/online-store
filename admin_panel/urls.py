5ea229d4a332ddbda97c90fe939e48d87b8a650c
Abdulrahman Abdulmuhsen <abdulrahmanyaf@gmail.com>
2022-11-24 21:19:26 +0300

    added ProductCategoryDelete functionality

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from admin_panel.views import ProductCategoryListView, ProductCategoryUpdateView, ProductCategoryCreateView
+from admin_panel.views import ProductCategoryListView, ProductCategoryUpdateView, ProductCategoryCreateView, \
+    ProductCategoryDeleteView
 
 app_name = 'admin_panel'
 urlpatterns = [
     path('product-categories/', ProductCategoryListView.as_view(), name='product_category_list'),
     path('product-categories/create', ProductCategoryCreateView.as_view(), name='product_category_create'),
-    path('product-categories/<int:pk>/', ProductCategoryUpdateView.as_view(), name='product_category_update'),
+    path('product-categories/<int:pk>/update/', ProductCategoryUpdateView.as_view(), name='product_category_update'),
+    path('product-categories/<int:pk>/delete/', ProductCategoryDeleteView.as_view(), name='product_category_delete'),
 ]
--- admin_panel/views.py
+++ admin_panel/views.py
@@ -1,7 +1,10 @@
+from django.contrib import messages
 from django.db.models import Count
+from django.http import HttpResponseBadRequest
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
-from django.views.generic import ListView, UpdateView, CreateView
+from django.views.generic import ListView, UpdateView, CreateView, DeleteView
+from django.utils.translation import gettext as _
 
 from products.models import ProductCategory
 
@@ -30,4 +33,23 @@ class ProductCategoryCreateView(CreateView):
     fields = ['title_en', 'title_ar', 'description_en', 'description_ar']
     template_name = 'admin_panel/product_categories/product_category_create.html'
     success_url = reverse_lazy('admin_panel:product_category_list')
+
+
+class ProductCategoryDeleteView(DeleteView):
+    model = ProductCategory
+    success_url = reverse_lazy('admin_panel:product_category_list')
+
+    def get_queryset(self):
+        return super().get_queryset().annotate(product_count=Count('products'))
+
+    def delete(self, request, *args, **kwargs):
+        product_category = self.get_object()
+        if product_category.product_count != 0:
+            return HttpResponseBadRequest()
+        response = super().delete(request, *args, **kwargs)
+        messages.success(request, _('Product category is deleted successfully'))
+        return response
+
+
+
 
